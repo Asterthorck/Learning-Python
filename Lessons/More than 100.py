9fdc44063e503b2c441bb171233107d02ca58daf
Asterthorck <99779731+Asterthorck@users.noreply.github.com>
2022-02-22 22:56:32 -0600

    Code created or tested on my python lessons

---
+++ Lessons/More than 100.py
@@ -0,0 +1,8 @@
+#test
+x = input('Place a number: ')
+y = int(x)
+if y > 1 :
+    print('More than one')
+    if y < 100 :
+        print('Less than 100')
+print('All Done')
---
+++ Lessons/Temperature Converter F to C - C to F.py
@@ -0,0 +1,17 @@
+x = input('Type 1 to convert from C to F or type 2 to convert from F to C: ')
+y = int(x)
+
+if y == 1 :
+    c = input('Enter C: ')
+    celcius = int(c)
+    celciusresult = (celcius * (9/5)) + 32
+    print(celciusresult,'F')
+
+if y == 2 :
+    f = input('Enter F: ')
+    fahrenheit = int(f)
+    fahrenheitresult = (fahrenheit - 32 ) * (5/9)
+    print(fahrenheitresult,'C')
+
+if y != 1 and y != 2 :
+    print('Error!, please type 1 to convert from C to F or type 2 to convert from F to C')
