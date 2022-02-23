8c42d3b87c542b04ef4fa9bdc7898b9711440452
Asterthorck <99779731+Asterthorck@users.noreply.github.com>
2022-02-22 23:01:30 -0600

    Update Temperature Converter F to C - C to F.py

--- Lessons/Temperature Converter F to C - C to F.py
+++ Lessons/Temperature Converter F to C - C to F.py
@@ -15,3 +15,4 @@ if y == 2 :
 
 if y != 1 and y != 2 :
     print('Error!, please type 1 to convert from C to F or type 2 to convert from F to C')
+#I intend to loop back at the begginin to give the user the option to try to intup again in case of placing a value different than 1 or 2
